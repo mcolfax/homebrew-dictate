b642df77334ba3ac2cc4f7ecaf69bcc3d6f56fd0
Matthew Colfax <mattcolfax@yahoo.com>
2026-04-03 09:47:45 -0400

    Update to v1.5.0

--- Casks/dictate.rb
+++ Casks/dictate.rb
@@ -1,6 +1,6 @@
 cask "dictate" do
-  version "1.4.9"
-  sha256 "e182b170b4b31da244adc6c62f7397199ff783f7654dc87a3454e5c9e50bcaa5"
+  version "1.5.0"
+  sha256 "12926a272c4a7c23dc8043fea9fab77d63f478a2416100dcb5679b840981533a"
 
   url "https://github.com/mcolfax/dictate/releases/download/v#{version}/Dictate.dmg"
   name "Dictate"
