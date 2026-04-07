74f85b8e6d1b67f923cff89dcda2d46a54b314af
mcolfax <38219569+mcolfax@users.noreply.github.com>
2026-04-07 14:16:02 -0400

    cask: update dictate to v1.6.0

--- Casks/dictate.rb
+++ Casks/dictate.rb
@@ -1,6 +1,6 @@
 cask "dictate" do
-  version "1.5.7"
-  sha256 "63be75963bda42aba6ea36fa3780af59ba43495bb8b4a4133dfb39af67b56e87"
+  version "1.6.0"
+  sha256 "bdeafe0eb96eeec4564ee8557a4e9d52fa2f7ea1467d948308a9696eebce6309"
 
   url "https://github.com/mcolfax/dictate/releases/download/v#{version}/Dictate.dmg"
   name "Dictate"
